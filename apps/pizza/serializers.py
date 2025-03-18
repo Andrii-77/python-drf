15a7774739605f055f1b4d9f34e10bd026cd3586
Andrii-77 <andrijsmaga@gmail.com>
2025-03-18 19:51:19 +0200

    Second commit and push python drf lesson2.

--- apps/pizza/serializers.py
+++ apps/pizza/serializers.py
@@ -3,19 +3,25 @@ from rest_framework import serializers
 from apps.pizza.models import PizzaModel
 
 
-class PizzaSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=20)
-    size = serializers.IntegerField()
-    price = serializers.FloatField()
-    created_at = serializers.DateTimeField(read_only=True)
-    updated_at = serializers.DateTimeField(read_only=True)
+class PizzaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PizzaModel
+        fields = ('id', 'name', 'size', 'price', 'updated_at', 'created_at')
 
-    def create(self, validated_data:dict):
-        return PizzaModel.objects.create(**validated_data)
-
-    def update(self, instance, validated_data:dict):
-        for k, v in validated_data.items():
-            setattr(instance, k, v)
-        instance.save()
-        return instance
+        # те, що прописане зверху, заміняє те, що знизу:
+        # fields = '__all__'
+    # id = serializers.IntegerField(read_only=True)
+    # name = serializers.CharField(max_length=20)
+    # size = serializers.IntegerField()
+    # price = serializers.FloatField()
+    # created_at = serializers.DateTimeField(read_only=True)
+    # updated_at = serializers.DateTimeField(read_only=True)
+    #
+    # def create(self, validated_data:dict):
+    #     return PizzaModel.objects.create(**validated_data)
+    #
+    # def update(self, instance, validated_data:dict):
+    #     for k, v in validated_data.items():
+    #         setattr(instance, k, v)
+    #     instance.save()
+    #     return instance
--- apps/pizza/views.py
+++ apps/pizza/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q, Max, Min, Count
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
@@ -7,8 +8,22 @@ from apps.pizza.serializers import PizzaSerializer
 
 class PizzaListCreateView(APIView):
     def get(self, *args, **kwargs):
-        pizzas = PizzaModel.objects.all()
-        serializer = PizzaSerializer(pizzas, many=True)
+        # pizzas = PizzaModel.objects.all()
+        # pizzas = pizzas.filter(Q(size=30) | Q(name='Roma'))
+        # pizzas = PizzaModel.objects.filter(Q(size=30) | Q(name='Roma')).exclude(price=1000).order_by('-size', '-price').reverse()
+        # pizzas = PizzaModel.objects.all()[0:10:3]
+        # pizzas = PizzaModel.objects.all().values('id', 'name', 'price')
+        # aggregate = PizzaModel.objects.aggregate(Min('size'), Max('size'))
+        # print(aggregate)
+        # annotate = PizzaModel.objects.values('size').annotate(count=Count('size'))
+        # print(annotate)
+        # print(pizzas.query)
+        # print(pizzas)
+        # return Response('OK')
+        # serializer = PizzaSerializer(pizzas, many=True)
+        # return Response(serializer.data, status.HTTP_200_OK)
+        qs = PizzaModel.objects.all()
+        serializer = PizzaSerializer(qs, many=True)
         return Response(serializer.data, status.HTTP_200_OK)
 
     def post(self, *args, **kwargs):
@@ -18,6 +33,7 @@ class PizzaListCreateView(APIView):
         serializer.save()
         return Response(serializer.data, status.HTTP_201_CREATED)
 
+
 class PizzaRetrieveUpdateDestroyView(APIView):
     def get(self, *args, **kwargs):
         pk = kwargs['pk']
@@ -25,7 +41,7 @@ class PizzaRetrieveUpdateDestroyView(APIView):
         try:
             pizza = PizzaModel.objects.get(pk=pk)
         except PizzaModel.DoesNotExist:
-            return Response({'Details':'Not found.'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'Details': 'Not found.'}, status=status.HTTP_404_NOT_FOUND)
 
         serializer = PizzaSerializer(pizza)
         return Response(serializer.data, status.HTTP_200_OK)
@@ -36,7 +52,7 @@ class PizzaRetrieveUpdateDestroyView(APIView):
         try:
             pizza = PizzaModel.objects.get(pk=pk)
         except PizzaModel.DoesNotExist:
-            return Response({'Details':'Not found.'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'Details': 'Not found.'}, status=status.HTTP_404_NOT_FOUND)
 
         data = self.request.data
         serializer = PizzaSerializer(pizza, data=data)
@@ -50,7 +66,6 @@ class PizzaRetrieveUpdateDestroyView(APIView):
         try:
             PizzaModel.objects.get(pk=pk).delete()
         except PizzaModel.DoesNotExist:
-            return Response({'Details':'Not found.'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'Details': 'Not found.'}, status=status.HTTP_404_NOT_FOUND)
 
         return Response(status=status.HTTP_204_NO_CONTENT)
-
